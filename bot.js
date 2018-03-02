5780c05eb0fed61eb32391ef97f36e025c0bd23f
Emma <36977619+vinechild@users.noreply.github.com>
2018-03-02 12:10:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,21 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+// Set the prefix
+const prefix = "j!";
+client.on("message", (message) => {
+    
+    //all my random shit
+    
+  if (message.content.toLowerCase().startsWith(prefix + "ping")) {
+    message.channel.send("Ping!");
+  } else
+  if (message.content.toLowerCase().startsWith(prefix + "foo")) {
+    message.channel.send("Uhh... fighters?");
+  } else
+  if (message.content.toLowerCase().startsWith(prefix + "cute")) {
+    message.channel.send("Ahahaha, thank you!");
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
